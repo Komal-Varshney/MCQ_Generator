8548f4296c9cc9c925b32d7d9eb97658c4a82fe9
Komal <vershneyk578@gmail.com>
2024-06-21 14:41:34 +0530

    Structure_updates

---
+++ src/mcq_generator/__init__.py
@@ -0,0 +1,4 @@
+# This src : we are gonna use it as our local folder
+# mcq_generator : this folder is considered as package
+# package : a folder containing multiple python files containng the code
+# and if we want to treat any folder as local package then there is a convention in python that we have to mention __init__.py file in that folder
