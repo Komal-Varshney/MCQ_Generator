de8514114d30e3ff2df391736806851701e412a1
Komal <vershneyk578@gmail.com>
2024-06-23 02:55:45 +0530

    key_updated

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 # for installing local package in virtual environment
 from setuptools import find_packages,setup
 
-setup{
-    
-}
+setup(
+    name = 'mcq_generator',
+    version='0.0.1',
+    author='Komal Varshney',
+    author_email='varshneyk578@gmail.com',
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    packages=find_packages()
+)
+#find_package is responsible for inding out the local packages whereever it will be able to find out init file it will consider that folder as local package
